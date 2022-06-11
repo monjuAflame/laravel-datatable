a46fd35da4dfd2f5846a6e26c42a654f1aa6560c
monjuAflame <monjuaflame@gmail.com>
2022-06-12 00:20:10 +0600

    show hide column

--- resources/views/customer/ajax.blade.php
+++ resources/views/customer/ajax.blade.php
@@ -18,9 +18,20 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
-        <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js" defer></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/fixedcolumns/3.3.3/css/fixedColumns.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css">
+
+        <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js" defer></script>
+
+        <script src="https://cdn.datatables.net/fixedcolumns/3.3.3/js/dataTables.fixedColumns.min.js"></script>
+
+        <script src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
+
+        <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.colVis.min.js"></script>
+        
 
 
     </head>
@@ -67,7 +78,10 @@
                         {"data": "first_name"},
                         {"data": "last_name"},
                         {"data": "email"},
-                    ]
+                    ],
+                    dom:            "Bfrtip",
+                    buttons:        [ 'colvis' ]
+
                 });
             } );
         </script>
