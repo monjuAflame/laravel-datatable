d1bd5f1606337e461938d978c1e2b2ee3fc9efe2
monjuAflame <monjuaflame@gmail.com>
2022-06-11 14:17:11 +0600

    all link added

--- resources/views/customer/datatable.blade.php
+++ resources/views/customer/datatable.blade.php
@@ -33,11 +33,7 @@
                         <div class="card-header">
                             <h2>Customer list</h2>
                             <br>
-                            <a href="{{ route('customers.index') }}" class="btn btn-primary btn-sm"> Simple Table </a>
-                            <a href="{{ route('customers.datatable') }}" class="btn btn-primary btn-sm"> Datatable </a>
-                            <a href="{{ route('customers.ajax') }}" class="btn btn-primary btn-sm"> Ajx </a>
-
-                            
+                            @include('customer.link')
                         </div>
                         <div class="card-body">
                             <table class="table" id="datatable">
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -31,10 +31,7 @@
                         <div class="card-header">
                             <h2>Customer list</h2>
                             <br>
-                            <a href="{{ route('customers.index') }}" class="btn btn-primary btn-sm"> Simple Table </a>
-                            <a href="{{ route('customers.datatable') }}" class="btn btn-primary btn-sm"> Datatable </a>
-                            <a href="{{ route('customers.ajax') }}" class="btn btn-primary btn-sm"> Ajx </a>
-
+                            @include('customer.link')
                             
                         </div>
                         <div class="card-body">
